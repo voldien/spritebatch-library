4587eb03bd50ed1a1752e4e1834a9bb57d4a20f1
Voldie <voldiekami@gmail.com>
2018-06-07 13:06:11 +0200

    Added init shader header.

---
+++ include/shader.h
@@ -0,0 +1,86 @@
+/**
+	Simple Sprite batch implementation using OpenGL.
+	Copyright (C) 2017  Valdemar Lindberg
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#ifndef _SB_SHADER_H_
+#define _SB_SHADER_H_ 1
+#include"def.h"
+
+/**
+ *
+ */
+typedef struct sb_shader_t {
+	unsigned int program;       /*	shader program. */
+	unsigned int flag;          /*  */
+} SBShader;
+
+#ifdef __cplusplus	/*	C++ environment	*/
+extern "C"{
+#endif
+
+/**
+ *	Load shader object from source glsl
+ *	source code.
+ * @param shad
+ * @param cvertexSource
+ * @param cfragmentSource
+ * @param cgeometry_source
+ * @return 0 if failed.
+ */
+extern SBDECLSPEC int SBAPIENTRY sbCreateShaderv(SBShader* SB_RESTRICT shad,
+        const char* SB_RESTRICT cvertexSource,
+        const char* SB_RESTRICT cfragmentSource,
+        const char* SB_RESTRICT cgeometry_source);
+
+/**
+ * Check if shader object is a valid shader object.
+ * @param shader
+ * @return non-zero if valid, zero otherwise.
+ */
+extern SBDECLSPEC int SBAPIENTRY sbIsShader(const SBShader* shader);
+
+/**
+ * Delete shader object.
+ * @param header
+ * @return 0 if failed
+ */
+extern SBDECLSPEC int SBAPIENTRY sbDeleteShaderProgram(SBShader* header);
+
+/**
+ * Compile
+ * @param source
+ * @param flag
+ * @return
+ */
+extern SBDECLSPEC int SBAPIENTRY sbCompileShaderSourcev(const char* source,
+        unsigned int flag);
+
+/**
+ * Shader compile log.
+ * @param program
+ * @param shaderflag
+ * @return
+ */
+extern SBDECLSPEC int SBAPIENTRY sbShaderCompileLog(unsigned int program,
+        unsigned int shaderflag);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
