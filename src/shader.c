fc0b4e4b98fb1d598d076deb512cad6f1bcd98de
Voldie <voldiekami@gmail.com>
2019-01-19 12:52:17 +0100

    Aded texture and shader implementation.

---
+++ src/shader.c
@@ -0,0 +1,192 @@
+#include"shader.h"
+#include"glfuncs.h"
+#include"shaders.h"
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+#include<stdlib.h>
+
+#ifdef GL_ES_VERSION_3_0
+	#undef GL_ES_VERSION_3_0
+	#include<GLES3/gl3.h>
+#elif defined(GL_ES_VERSION_2_0)
+	#undef GL_ES_VERSION_2_0
+	#include<GLES2/gl2.h>
+#else
+	#include<GL/gl.h>
+#endif
+
+unsigned int sbGetGLSLVersion(void){
+
+	unsigned int version;
+	char glstring[128] = {0};
+	char* wspac;
+
+	/*	Extract version number.	*/
+	strcpy(glstring, (const char*)glGetString(GL_SHADING_LANGUAGE_VERSION));
+	wspac = strstr(glstring, " ");
+	if(wspac){
+		*wspac = '\0';
+	}
+	if(strstr(glstring, ".") != NULL){
+		unsigned int major = (unsigned int)(glstring[0] - '0') * 100;
+		unsigned int minor = (unsigned int)(glstring[2] - '0') * 10;
+
+		return  major + minor;
+	}else{
+		version = (unsigned int)strtof(glstring, NULL) * 100;
+		return version;
+	}
+}
+
+int sbCreateShaderv(SBShader* shader, const char* cvertexSource,
+		const char* cfragmentSource, const char* cgeometrySource) {
+
+	int error;
+	int ver, fra, geo;
+
+	assert(shader);
+
+	/*	Create program	.*/
+	shader->program = spbGLCreateProgram();
+	if(shader->program < 0)
+		return 0;
+
+	/*	*/
+	ver = fra = geo = 0;
+	if(cvertexSource){
+		ver = sbCompileShaderSourcev(cvertexSource, GL_VERTEX_SHADER_ARB);
+		spbGLAttachShader(shader->program, ver);
+	}
+	if(cfragmentSource){
+		fra = sbCompileShaderSourcev(cfragmentSource, GL_FRAGMENT_SHADER_ARB);
+		spbGLAttachShader(shader->program, fra);
+	}
+#if defined(__gl_h_)
+	if(cgeometrySource){
+		geo = sbCompileShaderSourcev(cgeometrySource, GL_GEOMETRY_SHADER_ARB);
+		spbGLAttachShader(shader->program, geo);
+	}
+#endif
+
+	/*	*/
+	spbGLLinkProgram(shader->program);
+
+#if defined(__gl_h_)
+	error = sbShaderCompileLog(shader->program, GL_PROGRAM);
+	/*	if shader failed. clean up resources.	*/
+	if(!error){
+		sbDeleteShaderProgram(shader);
+		return error;
+	}
+
+#endif
+
+	/*	*/
+	spbGLValidateProgram(shader->program);
+
+	/*	detach shader object and release their resources.	*/
+	if(spbGLIsShader(ver)){
+		spbGLDetachShader(shader->program, ver);
+		spbGLDeleteShader(ver);
+	}
+	if(spbGLIsShader(ver)){
+		spbGLDetachShader(shader->program, fra);
+		spbGLDeleteShader(fra);
+	}
+	if(spbGLIsShader(ver)){
+		spbGLDetachShader(shader->program, geo);
+		spbGLDeleteShader(geo);
+	}
+
+	return 1;
+}
+
+int sbIsShader(const SBShader* shader){
+	return spbGLIsProgramARB(shader->program) == GL_TRUE;
+}
+
+int sbDeleteShaderProgram(SBShader* shader){
+	int error;
+
+	spbGLDeleteProgram(shader->program);
+
+	/*	Check if program is valid.  */
+	return spbGLIsProgramARB(shader->program) == GL_FALSE;
+}
+
+int sbCompileShaderSourcev(const char* source, unsigned int flag){
+
+	GLuint shader;
+	int status;
+	GLint cstatus;
+	GLint value;
+
+	const char* strcore;
+	char glversion[64];
+	const char* sources[3];
+	const int numsources = sizeof(sources) / sizeof(sources[0]);
+
+	/*	Check if core.	*/
+	glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &value);
+	strcore = (value == GL_CONTEXT_CORE_PROFILE_BIT) ? "core" : "";
+
+	/*	Create version string.	*/
+	memset(glversion, 0, sizeof(glversion));
+	sprintf(glversion, "#version %d %s\n", sbGetGLSLVersion(), strcore);
+
+	/*	*/
+	sources[0] = glversion;
+	sources[1] = gc_shader_def;
+	sources[2] = source;
+
+	printf(glversion);
+
+	/*	Compile*/
+	shader = spbGLCreateShader(flag);
+	spbGLShaderSourceARB(shader, numsources, sources, NULL);
+	spbGLCompileShaderARB(shader);
+
+	/*	*/
+	/*	Check for compiling errors.	*/
+	spbGLGetShaderiv(shader, GL_COMPILE_STATUS, &cstatus);
+	if (cstatus == GL_FALSE) {
+		if(flag == GL_VERTEX_SHADER_ARB)
+			fprintf(stderr, "vertex\n");
+		if(flag == GL_FRAGMENT_SHADER_ARB)
+			fprintf(stderr, "fragment\n");
+		GLchar infolog[2048];
+		spbGLGetShaderInfoLog(shader, sizeof(infolog), NULL, infolog);
+		fprintf(stderr, "%s\n", infolog);
+	}
+
+	return shader;
+}
+
+int sbShaderCompileLog(unsigned int program, unsigned int shaderflag) {
+	int lStatus,validate;
+
+	/* Check if shader	*/
+	if(spbGLIsProgram(program)){
+
+		char log[512];
+
+		/*	*/
+		spbGLGetProgramiv(program, GL_LINK_STATUS, &lStatus);
+		spbGLGetProgramiv(program, GL_VALIDATE_STATUS, &validate);
+
+		/*	*/
+		if(lStatus == GL_TRUE)
+			return 1;
+
+		/*	*/
+		if(lStatus == GL_FALSE){
+			spbGLGetProgramInfoLog(program, sizeof(log), NULL, log);
+			printf("\x1B[31m""Failed to compile shader\n%s\n", log);
+		}
+
+		return lStatus;
+	}else
+		return 0;
+}
+
---
+++ src/texture.c
@@ -0,0 +1,84 @@
+#include"texture.h"
+#include"glfuncs.h"
+#include<assert.h>
+
+#ifdef SPRITE_BATCH_EGL
+#include<EGL/egl.h>
+#include<EGL/eglext.h>
+#endif
+#ifdef GL_ES_VERSION_3_0
+	#undef GL_ES_VERSION_3_0
+	#include<GLES3/gl3.h>
+	#include<GLES3/gl3ext.h>
+	#include<GLES3/gl3platform.h>
+#elif defined(GL_ES_VERSION_2_0)
+	#undef GL_ES_VERSION_2_0
+	#include<GLES2/gl2.h>
+	#include<GLES2/gl2ext.h>
+	#include<GLES2/gl2platform.h>
+#else
+	#include<GL/gl.h>
+	#include<GL/glu.h>
+	#include<GL/glext.h>
+#endif
+
+
+SBTexture* sbCreateTexture(SBTexture* SB_RESTRICT texture, unsigned int target, int level,
+        int internalFormat, int width, int height, int border,
+        unsigned int format, unsigned int type, const void * SB_RESTRICT pixels) {
+
+	assert(texture);
+
+	texture->target = target;
+	texture->internalformat = (unsigned int) internalFormat;
+	texture->width = (unsigned int) width;
+	texture->height = (unsigned int) height;
+	texture->internalformat = format;
+	texture->type = type;
+
+	/*	Generate texture object.	*/
+	glGenTextures(1, &texture->texture);
+	glBindTexture(target, texture->texture);
+	glPixelStorei(GL_PACK_ALIGNMENT, 4);
+
+	/*	Create texture.	*/
+	switch (target) {
+		case GL_TEXTURE_2D:
+			glTexImage2D(target, level, internalFormat, width, height, border, format, type, pixels);
+			break;
+#ifndef GL_ES_VERSION_2_0
+		case GL_TEXTURE_1D:
+			glTexImage1D(target, level, internalFormat, width, border, format, type, pixels);
+			break;
+		case GL_TEXTURE_2D_ARRAY:
+			glTexStorage3D(target, level, texture->internalformat, width, height, texture->layer);
+			break;
+		case GL_TEXTURE_3D:
+			break;
+#endif
+		default:
+			break;
+	}
+
+	/*	Texture sampler modes.	*/
+	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(target, GL_TEXTURE_WRAP_R, GL_REPEAT);
+
+	glBindTexture(target, 0);
+	if (sbIsTexture(texture) != GL_TRUE)
+		return NULL;
+	else
+		return texture;
+}
+
+void sbDeleteTexture(SBTexture* texture){
+	if(sbIsTexture(texture))
+		glDeleteTextures(1, &texture->texture);
+}
+
+int sbIsTexture(const SBTexture* texture){
+	return glIsTexture(texture->texture) == GL_TRUE;
+}
